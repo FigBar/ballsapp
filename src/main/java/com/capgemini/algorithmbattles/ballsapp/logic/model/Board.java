8533e1cb501c6804cd561cfe7354c5b0b80e001b
FigBar <bartekfigiel98@gmail.com>
2018-06-07 17:13:01 +0200

    finished implementing evaluate method in Board class

--- src/main/java/com/capgemini/algorithmbattles/ballsapp/logic/model/Board.java
+++ src/main/java/com/capgemini/algorithmbattles/ballsapp/logic/model/Board.java
@@ -35,7 +35,7 @@ public class Board {
                 if (board[row][i] == board[row][i + 1] &&
                         board[row][i + 1] == board[row][i + 2] && board[row][i + 3] == board[row][i + 4]) {
 
-                    if (board[row][i].toString() == which)
+                    if (board[row][i].toString().equals(which))
                         return +10;
                     else if (board[row][i] != null)
                         return -10;
@@ -51,35 +51,46 @@ public class Board {
             while (i <= 5) {
                 if (board[i][col] == board[i + 1][col] && board[i + 1][col] == board[i + 2][col] &&
                         board[i + 2][col] == board[i + 3][col] && board[i + 3][col] == board[i + 4][col]) {
-                    if (board[i][col].toString() == which)
+                    if (board[i][col].toString().equals(which))
                         return +10;
 
-                    else if (board[0][col].toString() != null)
+                    else if (board[i][col] != null)
                         return -10;
                     i++;
                 }
 
             }
         }
-        
-        //Diagonals
-        if (board[0][0]==board[1][1] && board[1][1]==board[2][2])
-        {
-            if (board[0][0]==player)
-                return +10;
-            else if (board[0][0]==opponent)
-                return -10;
+        // Checking for diagonals going from top right corner to bottom left corner.
+        for (int row = 0; row < 6; row++) {
+            for (int col = 0; col < 6; col++) {
+                if (board[row][col] == board[row + 1][col + 1] && board[row + 1][col + 1] == board[row + 2][col + 2] && board[row + 2][col + 2] == board[row + 3][col + 3] &&
+                        board[row + 3][col + 3] == board[row + 4][col + 4]) {
+                    if (board[row][col].toString().equals(which))
+                        return +10;
+
+                    else if (board[row][col].toString() != null)
+                        return -10;
+                }
+            }
+
         }
 
-        if (board[0][2]==board[1][1] && board[1][1]==board[2][0])
-        {
-            if (board[0][2]==player)
-                return +10;
-            else if (board[0][2]==opponent)
-                return -10;
+        for (int row = 0; row < 6; row++) {
+            for (int col = 9; col >= 4; col--) {
+                if (board[row][col] == board[row + 1][col - 1] && board[row + 1][col - 1] == board[row + 2][col - 2] && board[row + 2][col - 2] == board[row + 3][col - 3] &&
+                        board[row + 3][col - 3] == board[row + 4][col - 4]) {
+                    if (board[row][col].toString().equals(which))
+                        return +10;
+
+                    else if (board[row][col].toString() != null)
+                        return -10;
+                }
+            }
+
         }
 
-
+        return 0;
     }
 
     public boolean isGameFinished() {
