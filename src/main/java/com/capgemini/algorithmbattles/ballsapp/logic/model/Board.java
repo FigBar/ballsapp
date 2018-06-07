10a3046d13e8b973852570668459d816d404221d
martkans <martkans@outlook.com>
2018-06-08 00:55:26 +0200

    I dont know

--- src/main/java/com/capgemini/algorithmbattles/ballsapp/logic/model/Board.java
+++ src/main/java/com/capgemini/algorithmbattles/ballsapp/logic/model/Board.java
@@ -42,32 +42,20 @@ public class Board {
             while (i <= 7) {
                 if (board[row][i] == board[row][i + 1] && board[row][i + 1] == board[row][i + 2] && board[row][i] != null) {
                     if (board[row][i].equals(player)) {
-                        if (i > 0 && i < 7) {
-                            if (board[row][i - 1] != null && board[row][i + 3] != null) {
-                                continue;
-                            } else {
-                                if (board[row][i - 1] != null) {
-                                    if (board[row][i - 1].equals(player.getOther())) {
-                                        return new BoardCell(row, i + 3, null);
-                                    } else
-                                        return new BoardCell(row, i - 1, null);
-                                }
-                            }
-                        } else if (i == 0) {
-                            if (board[row][i + 3] != null) {
-                                if (board[row][i + 3].toString().equals(player.getOther())) {
-                                    continue;
-                                } else {
+                        if (i > 0) {
+                            if (i < 7) {
+                                if (board[row][i - 1] == null)
+                                    return new BoardCell(row, i - 1, null);
+                                if (board[row][i + 3] == null) {
                                     return new BoardCell(row, i + 3, null);
                                 }
-                            }
-                        } else if (i == 7) {
-                            if (board[row][i - 1] != null) {
-                                if (board[row][i - 1].toString().equals(player.getOther())) {
-                                    continue;
-                                } else {
+                            } else {
+                                if (board[row][i - 1] == null)
                                     return new BoardCell(row, i - 1, null);
-                                }
+                            }
+                        } else {
+                            if (board[row][i + 3] == null) {
+                                return new BoardCell(row, i + 3, null);
                             }
                         }
                     }
@@ -83,32 +71,20 @@ public class Board {
             while (i <= 7) {
                 if (board[i][col] == board[i + 1][col] && board[i + 1][col] == board[i + 2][col] && board[i][col] != null) {
                     if (board[i][col].equals(player)) {
-                        if (i > 0 && i < 7) {
-                            if (board[i - 1][col] != null && board[i + 3][col] != null) {
-                                continue;
-                            } else {
-                                if (board[i - 1][col] != null) {
-                                    if (board[i - 1][col].equals(player.getOther())) {
-                                        return new BoardCell(i + 3, col, null);
-                                    } else
-                                        return new BoardCell(i - 1, col, null);
-                                }
-                            }
-                        } else if (i == 0) {
-                            if (board[i + 3][col] != null) {
-                                if (board[i + 3][col].equals(player.getOther())) {
-                                    continue;
-                                } else {
+                        if (i > 0) {
+                            if (i < 7) {
+                                if (board[i - 1][col] == null)
+                                    return new BoardCell(i - 1, col, null);
+                                if (board[i + 3][col] == null) {
                                     return new BoardCell(i + 3, col, null);
                                 }
-                            }
-                        } else if (i == 7) {
-                            if (board[i - 1][col] != null) {
-                                if (board[i - 1][col].equals(player.getOther())) {
-                                    continue;
-                                } else {
+                            } else {
+                                if (board[i - 1][col] == null)
                                     return new BoardCell(i - 1, col, null);
-                                }
+                            }
+                        } else {
+                            if (board[i + 3][col] == null) {
+                                return new BoardCell(i + 3, col, null);
                             }
                         }
                     }
@@ -116,84 +92,84 @@ public class Board {
                 i++;
             }
         }
-        // Checking for diagonals going from top left corner to bottom right corner.
-        for (int row = 0; row < 8; row++) {
-            for (int col = 0; col < 8; col++) {
-                if (board[row][col] != null) {
-                    if (board[row][col] == board[row + 1][col + 1] && board[row + 1][col + 1] == board[row + 2][col + 2] && board[row][col] != null) {
-                        if (row > 0 && col > 0) {
-                            if (row < 7 && col < 7) {
-                                if (board[row - 1][col - 1] != null && board[row + 3][col + 3] != null) {
-
-                                } else {
-                                    if (board[row - 1][col - 1] != null) {
-                                        if (board[row - 1][col - 1].equals(player.getOther())) {
-                                            return new BoardCell(row + 3, col + 3, null);
-                                        } else
-                                            return new BoardCell(row - 1, col - 1, null);
-                                    }
-                                }
-                            } else {
-                                if (board[row - 1][col - 1] != null) {
-                                    if (board[row - 1][col - 1].equals(player.getOther())) {
-
-                                    } else {
-                                        return new BoardCell(row - 1, col - 1, null);
-                                    }
-                                }
-                            }
-                        } else {
-                            if (board[row + 3][col + 3] != null) {
-                                if (board[row + 3][col + 3].equals(player.getOther())) {
-
-                                } else {
-                                    return new BoardCell(row + 3, col + 3, null);
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-
-        }
-        // Checking for diagonals going from top right corner to bottom left corner.
-        for (int row = 0; row < 8; row++) {
-            for (int col = 9; col >= 2; col--) {
-                if (board[row][col] != null) {
-                    if (board[row][col] == board[row + 1][col - 1] && board[row + 1][col - 1] == board[row + 2][col - 2] && board[row][col] != null) {
-                        if (row > 0 && col < 9) {
-                            if (row < 7 && col > 2) {
-                                if (board[row - 1][col + 1] != null && board[row + 3][col - 3] != null) {
-                                } else {
-                                    if (board[row - 1][col + 1] != null) {
-                                        if (board[row - 1][col + 1].equals(player.getOther())) {
-                                            return new BoardCell(row + 3, col - 3, null);
-                                        } else
-                                            return new BoardCell(row - 1, col + 1, null);
-                                    }
-                                }
-                            } else {
-                                if (board[row - 1][col + 1] != null) {
-                                    if (board[row - 1][col + 1].equals(player.getOther())) {
-
-                                    } else {
-                                        return new BoardCell(row - 1, col + 1, null);
-                                    }
-                                }
-                            }
-                        } else {
-                            if (board[row + 3][col - 3] != null) {
-                                if (board[row + 3][col - 3].equals(player.getOther())) {
-                                } else {
-                                    return new BoardCell(row + 3, col - 3, null);
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-
-        }
+//        // Checking for diagonals going from top left corner to bottom right corner.
+//        for (int row = 0; row < 8; row++) {
+//            for (int col = 0; col < 8; col++) {
+//                if (board[row][col] != null) {
+//                    if (board[row][col] == board[row + 1][col + 1] && board[row + 1][col + 1] == board[row + 2][col + 2] && board[row][col] != null) {
+//                        if (row > 0 && col > 0) {
+//                            if (row < 7 && col < 7) {
+//                                if (board[row - 1][col - 1] != null && board[row + 3][col + 3] != null) {
+//
+//                                } else {
+//                                    if (board[row - 1][col - 1] != null) {
+//                                        if (board[row - 1][col - 1].equals(player.getOther())) {
+//                                            return new BoardCell(row + 3, col + 3, null);
+//                                        } else
+//                                            return new BoardCell(row - 1, col - 1, null);
+//                                    }
+//                                }
+//                            } else {
+//                                if (board[row - 1][col - 1] != null) {
+//                                    if (board[row - 1][col - 1].equals(player.getOther())) {
+//
+//                                    } else {
+//                                        return new BoardCell(row - 1, col - 1, null);
+//                                    }
+//                                }
+//                            }
+//                        } else {
+//                            if (board[row + 3][col + 3] != null) {
+//                                if (board[row + 3][col + 3].equals(player.getOther())) {
+//
+//                                } else {
+//                                    return new BoardCell(row + 3, col + 3, null);
+//                                }
+//                            }
+//                        }
+//                    }
+//                }
+//            }
+//
+//        }
+//        // Checking for diagonals going from top right corner to bottom left corner.
+//        for (int row = 0; row < 8; row++) {
+//            for (int col = 9; col >= 2; col--) {
+//                if (board[row][col] != null) {
+//                    if (board[row][col] == board[row + 1][col - 1] && board[row + 1][col - 1] == board[row + 2][col - 2] && board[row][col] != null) {
+//                        if (row > 0 && col < 9) {
+//                            if (row < 7 && col > 2) {
+//                                if (board[row - 1][col + 1] != null && board[row + 3][col - 3] != null) {
+//                                } else {
+//                                    if (board[row - 1][col + 1] != null) {
+//                                        if (board[row - 1][col + 1].equals(player.getOther())) {
+//                                            return new BoardCell(row + 3, col - 3, null);
+//                                        } else
+//                                            return new BoardCell(row - 1, col + 1, null);
+//                                    }
+//                                }
+//                            } else {
+//                                if (board[row - 1][col + 1] != null) {
+//                                    if (board[row - 1][col + 1].equals(player.getOther())) {
+//
+//                                    } else {
+//                                        return new BoardCell(row - 1, col + 1, null);
+//                                    }
+//                                }
+//                            }
+//                        } else {
+//                            if (board[row + 3][col - 3] != null) {
+//                                if (board[row + 3][col - 3].equals(player.getOther())) {
+//                                } else {
+//                                    return new BoardCell(row + 3, col - 3, null);
+//                                }
+//                            }
+//                        }
+//                    }
+//                }
+//            }
+//
+//        }
 
         return null;
     }
